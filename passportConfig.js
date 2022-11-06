0746e9e5793956e709f5c046543f7f9c457cfea3
Aakash <aakash0977@gmail.com>
2022-11-06 23:14:06 +0545

    added all features

--- passportConfig.js
+++ passportConfig.js
@@ -2,45 +2,48 @@ const LocalStrategy = require('passport-local').Strategy;;
 const { pool } =   require('./dbConfig');
 const bcrypt = require('bcrypt');
 
-function initialize(passport1){
-    const authenticateUser  = (email, password,  done)=>{
-        pool.query(
-            `SELECT * FROM users WHERE email = $1`,
-            [email],(err, results)=> {
-                if(err){
-                    throw err;
-                }
-                console.log(results.rows);
-                if (results.rows.length > 0){
-                    const user = results.rows[0];
-                    
-                    bcrypt.compare(password, user.password, (err, isMatch)=>{
-                        if (err){
-                            throw err
-                        }
-                        if (isMatch){
-                            return done(null, user);
-                        }else{
-                            return done(null, false, {message: "Passwords do not match"});
-                        }
-                    });
-                }else{
-                    return done(null, false, {message: "Email is not registered"});
-                }
+function initialize(passport){
+const authenticateUser  = (req, email, password,  done)=>{
+    console.log(req.body);
+    req.body.is_admin = req.body.is_admin ? 1 : 0;
+    console.log(req.body);
+    pool.query(
+        `SELECT * FROM users WHERE email = $1 and is_admin=$2`,
+        [email,req.body.is_admin],(err, results)=> {
+            if(err){
+                throw err;
             }
-        );
-    };
+            console.log(results.rows);
+            if (results.rows.length > 0){
+                const user = results.rows[0];
+                
+                bcrypt.compare(password, user.password, (err, isMatch)=>{
+                    if (err){
+                        throw err
+                    }
+                    if (isMatch){
+                        return done(null, user);
+                    }else{
+                        return done(null, false, {message: "Passwords do not match"});
+                    }
+                });
+            }else{
+                return done(null, false, {message: "Email is not registered"});
+            }
+        }
+    );
+};
 
-    passport1.use(new LocalStrategy({
+    passport.use(new LocalStrategy({
         usernameField:"email",
         passwordField:"password",
-
+        passReqToCallback: true
     }, authenticateUser)
         );
     
-    passport1.serializeUser((user, done)=> done(null, user.id));
+    passport.serializeUser((user, done)=> done(null, user.id));
 
-    passport1.deserializeUser((id, done)=>{
+    passport.deserializeUser((id, done)=>{
         pool.query(
             `SELECT * FROM users WHERE id = $1`,[id], (err, results)=>{
                 if (err){
@@ -52,39 +55,4 @@ function initialize(passport1){
     });
 }
 
-// const authPage = (permissions) =>{
-    
-    
-//     return (req, res, done) =>{
-        
-//         pool.query(
-//             `SELECT * FROM users WHERE email = $1 and is_admin = $2`,
-//             [req.body.email,permissions],(err, results)=> {
-//                 if(err){
-//                     throw err;
-//                 }
-//                 console.log(results.rows);
-//                 if (results.rows.length > 0){
-//                     const user = results.rows[0];
-                    
-//                     bcrypt.compare(req.body.password, user.password, (err, isMatch)=>{
-//                         if (err){
-//                             throw err
-//                         }
-//                         if (isMatch){
-//                             return done(null, user);
-//                         }else{
-//                             return done(null, false, {message: "Passwords do not match"});
-//                         }
-//                     });
-//                 }else{
-//                     return done(null, false, {message: "Email is not registered"});
-//                 }
-//             }
-//         );
-//     }
-// }
-
-
-module.exports = {initialize};
-
+module.exports = initialize;
--- server.js
+++ server.js
@@ -8,23 +8,19 @@ const flash = require('express-flash');
 const passport = require('passport');
 const {pool} = require('./dbConfig');
 const nodemailer = require('nodemailer');
-const {initialize,authPage} = require('./passportConfig');
+require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
     service:"hotmail",
     auth:{
         user:"nodetest0977@hotmail.com",
-        pass:"Kathmandu@321"
+        pass: process.env.MAIL_PASSWORD
     }
 });
 
-
-// const initializePassport = require('./passportConfig');
-// const { request } = require('http');
-// initializePassport(passport);
-
+const initializePassport = require('./passportConfig');
 const { request } = require('http');
-initialize(passport);
+initializePassport(passport);
 
 const PORT = process.env.PORT || 4000;
 
@@ -49,8 +45,11 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
+app.get ('/', (req, res) => {
+    res.render('index');
+});
+
 app.get ('/home',checkNotAuthenticated, async (req, res) => {
-    const str = req.user;
     const allBlogPost = await pool.query(`SELECT b.ID,title,post,to_char(created_date,'yyyy-MM-dd') created_date,
     to_char(updated_date,'yyyy-MM-dd') updated_date,
     (Select count(1) from tbl_blog_like where blog_id=b.ID) LikeCount,
@@ -61,129 +60,10 @@ app.get ('/home',checkNotAuthenticated, async (req, res) => {
     const allBlogComment = await pool.query(`Select c.ID,blog_id,comment,user_id,u.name
 	from tbl_blog_comment c
 	left join users u on u.ID=c.user_id`);
-    res.render('home',  {user: str,posts: allBlogPost.rows, comments: allBlogComment.rows});
+    res.render('home',  {user: req.user, posts: allBlogPost.rows, comments:allBlogComment.rows});
 });
-
-
-app.get ('/', (req, res) => {
-    res.render('index');
-});
-
-app.get ('/register', checkAuthenticated, (req, res) => {
-    res.render('register');
-});
-
-app.get ('/login',checkAuthenticated, (req, res) => {
-    res.render('login');
-});
-
-app.get ('/profile', checkNotAuthenticated, (req, res) => {
-    const str = req.user;
-    res.render('profile', {user: str});
-});
-
-app.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-          res.status(400).send('Unable to log out')
-        } else {
-            res.redirect('/login');
-        }
-      });
-
-});
-
-app.get ('/adminlogin', (req, res) => {
-    res.render('adminLogin');
-});
-
-app.post ('/adminlogin', (req, res) => {
-    pool.query(
-        `SELECT * FROM users WHERE email = $1 and is_admin = true`,
-        [req.body.email],(err, results)=> {
-            console.log(results.rows);
-            if (results.rows.length > 0){
-                const user = results.rows[0];
-                
-                bcrypt.compare(req.body.password, user.password, (err, isMatch)=>{
-                    if (err){
-                        req.flash("error", "Password mismatch");
-                        res.redirect('/adminLogin');
-                    }
-                    else{
-                        res.redirect('/adminHome');
-                    }
-                });
-            }else{
-                 req.flash("error", "Email is not admin");
-                 res.redirect('/adminLogin');
-            }
-        }
-    );
-    
-    
-});
-
-app.get ('/adminhome', async (req, res) => {
-    const allBlogPost = await pool.query(`SELECT b.ID,title,post,to_char(created_date,'yyyy-MM-dd') created_date,
-    to_char(updated_date,'yyyy-MM-dd') updated_date,
-    (Select count(1) from tbl_blog_like where blog_id=b.ID) LikeCount,
-	(Select count(1) from tbl_blog_comment where blog_id=b.ID) CommentCount
-    FROM tbl_blog b`);
-    
-    const allBlogComment = await pool.query(`Select c.ID,blog_id,comment,user_id,u.name
-	from tbl_blog_comment c
-	left join users u on u.ID=c.user_id`);
-    
-    //const str2 = str.charAt(0).toUpperCase()+str.slice(1);
-    res.render('adminHome',  {posts: allBlogPost.rows, comments: allBlogComment.rows});
-});
-
-app.get ('/addblog', (req, res) => {
-    res.render('addBlog');
-});
-
-//post comments
-app.post('/sendmail', checkNotAuthenticated,(req, res) => {
-
-    if (req.body.comments.length == 0) {
-        
-        res.redirect('/home');
-    }
-    const hasCommented = pool.query(`Select 'm' from tbl_blog_comment where user_id = $2 and blog_id = $1`,[req.body.blogId, req.user.id])
-    console.log(hasCommented);
-    if(hasCommented.length>0) {
-        res.flash('error', 'You already have a commented');
-        res.redirect('/home');
-    }
-    else{
-        pool.query(`Insert into tbl_blog_comment(blog_id,user_id,comment,created_date,updated_date)
-        values ($1,$2,$3,now(),now())`,[req.body.blogId, req.user.id, req.body.comments])
-        res.redirect('/home');
-        // const options ={
-        //     from:"nodetest0977@hotmail.com",
-        //     to:"aakash0977@gmail.com",
-        //     subject:"Sending test email",
-        //     text: req.user.name+" commented "+ req.body.comments +" on your blog"
-        // }
-    
-        // transporter.sendMail(options, (err,info)=>{
-        //     if(err){
-        //         console.log(err);
-        //         return;
-        //     }
-            
-        //     res.redirect('/home');
-        // })
-    }
-
-
-});
-
 
 app.post ('/home',checkNotAuthenticated, async (req, res) => {
-    const str = req.user;
-    console.log(str);
     const allBlogPost = await pool.query(`SELECT b.ID,title,post,to_char(created_date,'yyyy-MM-dd') created_date,
     to_char(updated_date,'yyyy-MM-dd') updated_date,
     (Select count(1) from tbl_blog_like where blog_id=b.ID) LikeCount,
@@ -195,51 +75,13 @@ app.post ('/home',checkNotAuthenticated, async (req, res) => {
     const allBlogComment = await pool.query(`Select c.ID,blog_id,comment,user_id,u.name name
 	from tbl_blog_comment c
 	left join users u on u.ID=c.user_id`);
-
-    //const str2 = str.charAt(0).toUpperCase()+str.slice(1);
-    res.render('home',  {user: str,posts: allBlogPost.rows, comments: allBlogComment.rows});
+    
+    res.render('home',  {user: req.user, posts: allBlogPost.rows, comments: allBlogComment.rows});
 });
 
-
-app.get('/like/:blogId',checkNotAuthenticated, async (req, res) => {
-    console.log(req.params.blog);
-    const is_liked = (await pool.query(`Select 'm' from tbl_blog_like where user_id=$2 and blog_id=$1`,[req.params.blogId, req.user.id]))
-    if(is_liked.rowCount > 0){
-        await pool.query(`Delete from tbl_blog_like where blog_id=$1 and user_id=$2`,[req.params.blogId, req.user.id])
-    }
-    else{
-        await pool.query(`Insert into tbl_blog_like(blog_id,user_id,created_date,updated_date)
-        values
-        ($1,$2,now(),now())`,[req.params.blogId, req.user.id])
-    }
-    res.redirect('/home');
-})
-
-app.get('/comment/:blogId',checkNotAuthenticated, async (req, res) => {
-    const str = req.user;
-    const allBlogPost = await pool.query(`SELECT b.ID,title,post,to_char(created_date,'yyyy-MM-dd') created_date,
-    to_char(updated_date,'yyyy-MM-dd') updated_date,
-    (Select count(1) from tbl_blog_like where blog_id=b.ID) LikeCount,
-	(Select count(1) from tbl_blog_comment where blog_id=b.ID) CommentCount,
-    coalesce((Select 'y' from tbl_blog_like where blog_id=b.ID and user_id= $1),'n') is_liked
-    FROM tbl_blog b where id=$2`,[req.user.id,req.params.blogId]);
-    
-    const allBlogComment = await pool.query(`Select c.ID,blog_id,comment,user_id,u.name
-	from tbl_blog_comment c
-	left join users u on u.ID=c.user_id
-    where blog_id=$1`,[req.params.blogId]);
-    console.log(allBlogComment.rows);
-
-    //const hasCommented = await pool.query(`Select id from tbl_blog_comment where user_id = $2 and blog_id=$1`,[req.params.blogId, req.user.id]);
-
-    res.render('post',  {user: str,posts: allBlogPost.rows, comments: allBlogComment.rows});
-})
-
-app.get('/delcomment/:blogId/:commentId',checkNotAuthenticated, (req, res) => {
-    pool.query(`Delete from tbl_blog_comment where id=$1 and user_id=$2`,[req.params.commentId,req.user.id])
-    res.redirect('/comment/'+req.params.blogId);
-})
-
+app.get ('/register', checkAuthenticated, (req, res) => {
+    res.render('register');
+});
 
 app.post('/register', async (req, res) => {
     let {name, email, password, password2 } = req.body;
@@ -278,8 +120,8 @@ app.post('/register', async (req, res) => {
                 res.render('register',{errors});
             }else{
                 pool.query(
-                    `INSERT INTO users (name, email, password) 
-                    VALUES ($1, $2, $3)
+                    `INSERT INTO users (name, email, password, is_admin) 
+                    VALUES ($1, $2, $3, false)
                     RETURNING id, password`,
                     [name,email,hashedPassword],
                     (err, results) => {
@@ -297,71 +139,24 @@ app.post('/register', async (req, res) => {
 
 });
 
+
+app.get ('/login',checkAuthenticated, (req, res) => {
+    res.render('login');
+});
+
 app.post("/login", passport.authenticate('local', {
     successRedirect:"/home",
     failureRedirect:"/login",
     failureFlash:true 
-})
+    }),(req, res)=>{
+        res.next();
+    }
 );
 
-
-app.post('/addBlog', async (req, res) => {
-    pool.query(`INSERT INTO public.tbl_blog(
-        title, post, created_date, updated_date)
-        VALUES ( $1, $2, now(), now());`,[req.body.title,req.body.post]);
-        res.redirect('/adminhome');
-});
-
-app.get('/editBlog/:blogId', async (req, res) =>{
-    const BlogPost = await pool.query(`Select * from tbl_blog where id=$1`,[req.params.blogId]);
-    console.log(BlogPost.rows);
-    res.render('editBlog',  {editpost: BlogPost.rows[0]});
-}
-);
-
-app.post('/editBlog', async (req, res) => {
-    await pool.query(`Update tbl_blog set title=$1, post=$2 where id=$3 `,[req.body.title,req.body.post,req.body.blogId])
-    res.redirect('/adminhome');
-});
-
-app.get('/deleteBlog/:blogId', async (req, res) => {
-    await pool.query(`Delete from tbl_blog where id=$1 `,[req.params.blogId])
-    res.redirect('/adminhome');
-});
-
-app.get('/admincomment/:blogId', async (req, res) => {
+app.get ('/profile', checkNotAuthenticated, (req, res) => {
     const str = req.user;
-    console.log(str);
-    const allBlogPost = await pool.query(`SELECT b.ID,title,post,to_char(created_date,'yyyy-MM-dd') created_date,
-    to_char(updated_date,'yyyy-MM-dd') updated_date,
-    (Select count(1) from tbl_blog_like where blog_id=b.ID) LikeCount,
-	(Select count(1) from tbl_blog_comment where blog_id=b.ID) CommentCount
-    FROM tbl_blog b where id=$1`,[req.params.blogId]);
-    
-    const allBlogComment = await pool.query(`Select c.ID,blog_id,comment,user_id,u.name
-	from tbl_blog_comment c
-	left join users u on u.ID=c.user_id
-    where blog_id=$1`,[req.params.blogId]);
-
-    res.render('adminpost',  {user: str,posts: allBlogPost.rows, comments: allBlogComment.rows});
-})
-
-app.get('/admindelcomment/:blogId/:commentId', (req, res) => {
-    pool.query(`Delete from tbl_blog_comment where id=$1`,[req.params.commentId])
-    res.redirect('/admincomment/'+req.params.blogId);
-})
-
-app.get('/userdetails',async (req,res)=>{
-    const allusers = await pool.query(`SELECT id, name, email, password
-	FROM public.users `);
-    res.render('userDetails',{posts:allusers.rows});
-})
-
-app.get('/deluser/:userId', (req, res) => {
-    pool.query(`Delete from users where id=$1`,[req.params.userId])
-    res.redirect('/userdetails');
-})
-
+    res.render('profile', {user: str});
+});
 
 app.post('/profile', async (req, res) => {
     let {userid,name, email, password, password2 } = req.body;
@@ -419,6 +214,158 @@ app.post('/profile', async (req, res) => {
 
 });
 
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+          res.status(400).send('Unable to log out')
+        } else {
+            res.redirect('/login');
+        }
+      });
+
+});
+
+//post comments
+app.post('/comment', checkNotAuthenticated,(req, res) => {
+
+        pool.query(`Insert into tbl_blog_comment(blog_id,user_id,comment,created_date,updated_date)
+        values ($1,$2,$3,now(),now())`,[req.body.blogId, req.user.id, req.body.comments])
+        res.redirect('/home');
+
+        const options ={
+            from:"nodetest0977@hotmail.com",
+            to:"aakash0977@gmail.com",
+            subject:"Sending test email",
+            text: req.user.name+" commented "+ "'" + req.body.comments + "' " + "on blog '" + req.body.blogTitle+"'"
+        }
+        transporter.sendMail(options, (err,info)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            console.log("Mail sent")
+        })
+});
+
+app.get('/comment/:blogId',checkNotAuthenticated, async (req, res) => {
+    const allBlogPost = await pool.query(`SELECT b.ID,title,post,to_char(created_date,'yyyy-MM-dd') created_date,
+    to_char(updated_date,'yyyy-MM-dd') updated_date,
+    (Select count(1) from tbl_blog_like where blog_id=b.ID) LikeCount,
+	(Select count(1) from tbl_blog_comment where blog_id=b.ID) CommentCount,
+    coalesce((Select 'y' from tbl_blog_like where blog_id=b.ID and user_id= $1),'n') is_liked
+    FROM tbl_blog b where id=$2`,[req.user.id,req.params.blogId]);
+    
+    const allBlogComment = await pool.query(`Select c.ID,blog_id,comment,user_id,u.name
+	from tbl_blog_comment c
+	left join users u on u.ID=c.user_id
+    where blog_id=$1`,[req.params.blogId]);
+
+    res.render('post',  {user: req.user, posts: allBlogPost.rows, comments: allBlogComment.rows});
+})
+
+app.get('/like/:blogId',checkNotAuthenticated, async (req, res) => {
+    const is_liked = (await pool.query(`Select 'm' from tbl_blog_like where user_id=$2 and blog_id=$1`,[req.params.blogId, req.user.id]))
+    if(is_liked.rowCount > 0){
+        await pool.query(`Delete from tbl_blog_like where blog_id=$1 and user_id=$2`,[req.params.blogId, req.user.id])
+    }
+    else{
+        await pool.query(`Insert into tbl_blog_like(blog_id,user_id,created_date,updated_date)
+        values
+        ($1,$2,now(),now())`,[req.params.blogId, req.user.id])
+    }
+    res.redirect('/home');
+})
+
+app.get('/delcomment/:blogId/:commentId',checkNotAuthenticated, (req, res) => {
+    pool.query(`Delete from tbl_blog_comment where id=$1 and user_id=$2`,[req.params.commentId,req.user.id])
+    res.redirect('/comment/'+req.params.blogId);
+})
+
+app.get ('/adminlogin', (req, res) => {
+    res.render('adminLogin');
+});
+
+app.post ('/adminlogin', passport.authenticate('local', {
+    successRedirect:"/adminhome",
+    failureRedirect:"/adminlogin",
+    failureFlash:true 
+}), (req, res) =>{
+    next();
+});
+
+app.get ('/adminhome', checkNotAuthenticatedAdmin,  async (req, res) => {
+    const allBlogPost = await pool.query(`SELECT b.ID,title,post,to_char(created_date,'yyyy-MM-dd') created_date,
+    to_char(updated_date,'yyyy-MM-dd') updated_date,
+    (Select count(1) from tbl_blog_like where blog_id=b.ID) LikeCount,
+	(Select count(1) from tbl_blog_comment where blog_id=b.ID) CommentCount
+    FROM tbl_blog b`);
+    
+    const allBlogComment = await pool.query(`Select c.ID,blog_id,comment,user_id,u.name
+	from tbl_blog_comment c
+	left join users u on u.ID=c.user_id`);
+    res.render('adminHome',  {posts: allBlogPost.rows, comments: allBlogComment.rows});
+});
+
+app.get ('/addblog', checkNotAuthenticatedAdmin, (req, res) => {
+    res.render('addBlog');
+});
+
+
+
+app.post('/addBlog', async (req, res) => {
+    pool.query(`INSERT INTO public.tbl_blog(
+        title, post, created_date, updated_date)
+        VALUES ( $1, $2, now(), now());`,[req.body.title,req.body.post]);
+        res.redirect('/adminhome');
+});
+
+app.get('/editBlog/:blogId', async (req, res) =>{
+    const BlogPost = await pool.query(`Select * from tbl_blog where id=$1`,[req.params.blogId]);
+    res.render('editBlog',  {editpost: BlogPost.rows[0]});
+}
+);
+
+app.post('/editBlog', async (req, res) => {
+    await pool.query(`Update tbl_blog set title=$1, post=$2 where id=$3 `,[req.body.title,req.body.post,req.body.blogId])
+    res.redirect('/adminhome');
+});
+
+app.get('/deleteBlog/:blogId', async (req, res) => {
+    await pool.query(`Delete from tbl_blog where id=$1 `,[req.params.blogId])
+    res.redirect('/adminhome');
+});
+
+app.get('/admincomment/:blogId', checkNotAuthenticatedAdmin, async (req, res) => {
+    const allBlogPost = await pool.query(`SELECT b.ID,title,post,to_char(created_date,'yyyy-MM-dd') created_date,
+    to_char(updated_date,'yyyy-MM-dd') updated_date,
+    (Select count(1) from tbl_blog_like where blog_id=b.ID) LikeCount,
+	(Select count(1) from tbl_blog_comment where blog_id=b.ID) CommentCount
+    FROM tbl_blog b where id=$1`,[req.params.blogId]);
+    
+    const allBlogComment = await pool.query(`Select c.ID,blog_id,comment,user_id,u.name
+	from tbl_blog_comment c
+	left join users u on u.ID=c.user_id
+    where blog_id=$1`,[req.params.blogId]);
+
+    res.render('adminpost',  {user: req.user, posts: allBlogPost.rows, comments: allBlogComment.rows});
+})
+
+app.get('/admindelcomment/:blogId/:commentId', (req, res) => {
+    pool.query(`Delete from tbl_blog_comment where id=$1`,[req.params.commentId])
+    res.redirect('/admincomment/'+req.params.blogId);
+})
+
+app.get('/userdetails',checkNotAuthenticatedAdmin, async (req,res)=>{
+    const allusers = await pool.query(`SELECT id, name, email, password
+	FROM public.users `);
+    res.render('userDetails',{posts:allusers.rows});
+})
+
+app.get('/deluser/:userId', (req, res) => {
+    pool.query(`Delete from users where id=$1`,[req.params.userId])
+    res.redirect('/userdetails');
+})
+
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return res.redirect('/home');
@@ -431,6 +378,13 @@ function checkNotAuthenticated(req, res, next) {
         return next();
     }
     res.redirect('/login');
+}
+
+function checkNotAuthenticatedAdmin(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/adminlogin');
 }
 
 app.listen(PORT, ()=> {
